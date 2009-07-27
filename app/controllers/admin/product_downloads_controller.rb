b69ceba7628af84b344151e3f5cb010395a590c8
chtrinh <chris.chtrinh@gmail.com>
2009-07-27 13:37:38 -0700

    Fixed merge error

--- app/controllers/admin/product_downloads_controller.rb
+++ app/controllers/admin/product_downloads_controller.rb
@@ -1,14 +1,9 @@
 class Admin::ProductDownloadsController < Admin::BaseController
-
   resource_controller
-<<<<<<< HEAD:app/controllers/admin/product_downloads_controller.rb
   # note: we're using attribute_fu to manage the product_properties so the products controller will be 
   # doing most of the work
   belongs_to :product
   
-=======
-  belongs_to :product
->>>>>>> 5a91be7bf3d8b207465dc8688438fea192118b88:app/controllers/admin/product_downloads_controller.rb
   # after_filter :set_image, :only => [:create, :update]
    
   # 
@@ -21,13 +16,6 @@ class Admin::ProductDownloadsController < Admin::BaseController
   #   option_values.each_value {|id| @object.option_values << OptionValue.find(id)}
   #   @object.save
   # end
-  # 
-<<<<<<< HEAD:app/controllers/admin/product_downloads_controller.rb
-  # # redirect to index (instead of r_c default of show view)
-  # create.response do |wants| 
-  #   wants.html {redirect_to collection_url}
-  # end
-  # 
   
   
   create do
@@ -44,20 +32,13 @@ class Admin::ProductDownloadsController < Admin::BaseController
     if object.product.product_downloads.count > 1
       object.product.bundle
     end
-=======
-  
-  # redirect to index (instead of r_c default of show view)
-  create.response do |wants| 
-    wants.html {redirect_to collection_url}
->>>>>>> 5a91be7bf3d8b207465dc8688438fea192118b88:app/controllers/admin/product_downloads_controller.rb
   end
   
   # redirect to index (instead of r_c default of show view)
   update.response do |wants| 
     wants.html {redirect_to collection_url}
   end
-  
-<<<<<<< HEAD:app/controllers/admin/product_downloads_controller.rb
+
   update.after do 
     #object:ProductDownload
     if object.product.product_downloads.count > 1
@@ -80,7 +61,4 @@ class Admin::ProductDownloadsController < Admin::BaseController
      flash[:notice] =  product.bundle ? 'Downloadable files were successfully zipped.' : 'No downloadable files to zip!'
      redirect_to collection_url
   end
-  
-=======
->>>>>>> 5a91be7bf3d8b207465dc8688438fea192118b88:app/controllers/admin/product_downloads_controller.rb
 end
